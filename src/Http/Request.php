cea57eb993fc4515900a856ff8344ad56b7df34c
huacaizhi <huacaizhi@gmail.com>
2019-07-16 00:33:33 +0800

    fix getRequestUrl cli mode

--- src/Http/Request.php
+++ src/Http/Request.php
@@ -6,17 +6,27 @@
  * Date: 2019/7/15
  * Time: 19:54
  */
+
 namespace HuaCaiZhi\PhpTools\Http;
 
 class Request
 {
     /**
      * 获取当前请求完整`URL`地址
-     *
+     * 注意`运行在命令行下`
      * @return string
      */
     public static function getRequestUrl()
     {
-        return $_SERVER["REQUEST_SCHEME"] . '://' . $_SERVER["SERVER_NAME"] . $_SERVER["REQUEST_URI"];
+        if (strpos(php_sapi_name(), 'cli') !== false) {
+            return realpath(__FILE__);
+        }
+        if (isset($_SERVER["REQUEST_SCHEME"])
+            && isset($_SERVER["SERVER_NAME"])
+            && isset($_SERVER["REQUEST_URI"])) {
+            return $_SERVER["REQUEST_SCHEME"] . '://' . $_SERVER["SERVER_NAME"] . $_SERVER["REQUEST_URI"];
+        }
+
+        return realpath(__FILE__);
     }
 }
